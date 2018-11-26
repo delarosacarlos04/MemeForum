d73482312d405fdcc87e989925bef0cdaf09e233
arianaolivia <ari667@yahoo.com>
2018-11-26 15:58:09 -0600

    added the username cookie
    you can call it elsewhere on the site, the key is "username"

--- login.php
+++ login.php
@@ -1,4 +1,3 @@
-<!DOCTYPE html>
 <?php
 $servername="localhost";
 $username="crypzzhj";
@@ -6,33 +5,26 @@ $password="D7iqck9yZMdr";
 $dbname="crypzzhj_userlogin";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 if(isset($_POST['Login'])){
-$username=$_POST['user'];
-$password=$_POST['pass'];
-$usertype=$_POST['usertype'];
-$query="SELECT * FROM userlogin WHERE username='".$username."' and password='".$password."' and usertype='".$usertype."'";
-$result = mysqli_query($conn, $query);
-if($result==TRUE){
-    if($row = mysqli_fetch_array($result)){
-        echo '<script type="text/javascript">alert("Success! You are logged in as '. $username . ' and you have *'.$row['usertype'].'* access")</script>';
-if($usertype=="admin"){
-?>
-<script type="text/javascript">
-    window.location.href = "RPS_game_admin.html";
-</script>
-<?php
-}
-if($usertype=="user"){
-    ?>
-    <script type="text/javascript">
-        window.location.href = "RPS_game.php";
-    </script>
-    <?php
-}
-}
+    $username=$_POST['user'];
+    $password=$_POST['pass'];
+    $usertype=$_POST['usertype'];
+    $query="SELECT * FROM userlogin WHERE username='".$username."' and password='".$password."' and usertype='".$usertype."'";
+    $result = mysqli_query($conn, $query);
+    if($result==TRUE){
+        if($row = mysqli_fetch_array($result)){
+            setcookie("username", $username);
+            echo '<script type="text/javascript">alert("Success! You are logged in as '. $username . '")</script>';
+            exit();
+        ?>
+        <script type="text/javascript">
+            window.location.href = "index.html";
+        </script>
+        <?php
+        }
+    }
 
 else{
     echo 'Password and username do not match. Please try again or create a new account.'; //create new not yet finished
-}
 }
 }
 ?>
@@ -63,6 +55,5 @@ else{
             <td><input type="submit" name="Login" value="Login"></td>
         </tr>
 </form>
-    On the next page you can upload an image file! </br>
 </body>
 </html>

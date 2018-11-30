7e192ce18eee37b766d81d98ee4c97dcee759ef2
arianaolivia <ari667@yahoo.com>
2018-11-30 15:29:59 -0600

    doesn't submit blank text posts

--- text_upload.php
+++ text_upload.php
@@ -8,15 +8,18 @@ $username=$_COOKIE['username'];
 $text=$_POST['text_path'];
 $time=time();
 if(isset($_POST['Submit'])){
-        $query= "INSERT INTO posts(time, username, text_path) VALUES ('".$time."', '".$username."', '".$text."')";
-        mysqli_query($query,$conn);
-        if($conn->query($query)===TRUE){
-            echo "yay";
-        }else{
-            echo "Error: " . $query . "<br>" . $conn->error;
-        }
-        
-        header("Location:/Meme_Forum");
-        //exit();
+    $query= "INSERT INTO posts(time, username, text_path) VALUES ('".$time."', '".$username."', '".$text."')";
+    mysqli_query($query,$conn);
+    if($conn->query($query)===TRUE){
+        echo "yay";
+    }else{
+        echo "Error: " . $query . "<br>" . $conn->error;
+    }
+    
+    header("Location:/Meme_Forum");
+    //exit();
+}
+if ($text === ''){
+    header("Location:/Meme_Forum/test.html");
 }
 ?>

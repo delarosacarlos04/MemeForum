6c972e8f4f9a3826267cbaba0026e1d9c68f05de
arianaolivia <ari667@yahoo.com>
2018-12-05 16:34:38 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,123 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Meme Forum!</title>
+    
+     <link rel="stylesheet" href="main.css" />
+     <link href="https://fonts.googleapis.com/css?family=Lora" rel="stylesheet"> 
+     
+</head>
+<body>
+     
+     
+      <div class="logs">
+       <div class="userOptions-group">
+            <button class="logButton" onclick="location.href='login.php'">Login</button>
+            <button class="logButton" onclick="location.href='logout.php'">Logout</button>
+        </div>
+    </div>
+    
+  
+  <script src="main.js"></script>
+    <div class="userTag">  
+        <script>
+            //Carlos: testing with a hard coded cookie
+            //document.cookie = "username = Carlos"; 
+            var name = getCookie("username");
+            document.write("<h3>User: ", name,"</h3>");
+        </script>
+   </div>   
+  
+  
+   <!-- <h1>Welcome to the Meme Forum! </h1>  -->
+    
+    <br><br><br><br>
+    
+    
+    <div class="banner" >
+    <img class="banner" src="assets/newBanner.png" alt="banner of multiple memes"> 
+    </div>
+
+
+   
+   <br><br>
+   
+    <div class="memeWindow">
+        <img class="memeWindow"  src="assets/pepeFeelsGood.png" alt="FeelsGoodMan Pepe image"> 
+    </div>
+          
+   
+   
+<div class="grid">
+             
+        <div class="userOptions-group">    
+            <br>
+            
+                <form action="text.html">
+                    <button class="button" >Text post</button>
+                </form>
+                
+                <form action="upload.html">
+                    <button class="button">Photo post</button>
+                </form>
+                
+            
+              <form>
+                <!--
+                <button class="button" onclick="location.href='text.html'">Text post</button>
+                <button class="button" onclick="location.href='upload.html'">Photo post</button>
+                
+                -->
+                <button class="button" >Randomize</button>
+                
+                <div class="dropdown">
+                    <button class="dropbtn">Filter</button>
+                    <div id="filterDropdown" class="dropdown-content">
+                        <a href="#">Option 1</a>
+                        <a href="#">Option 2</a>
+                    </div>
+                </div>
+            </form>
+
+            
+        </div>
+    </div>
+    
+<?php
+$connection = mysqli_connect('localhost', 'crypzzhj', 'D7iqck9yZMdr', 'crypzzhj_userlogin');
+$replyResult = mysqli_query($connection,"SELECT * FROM replies");
+$result = mysqli_query($connection,"SELECT * FROM posts");
+//$uniqueID = hash('crc32', $row['time']);
+while($row = mysqli_fetch_array($result)){
+    echo "Time Posted: " ,$row['time'] . "<br>";
+    echo "Username: " ,$row['username'] . "<br>";
+    echo "Post: " ,$row['text_path'] . "<br>";
+    echo "ID: ", $uniqueID;
+    getReplies('0', $row['ID']);
+/*    if ($row = mysqli_fetch_array($replyResult)){
+        $replies = mysqli_fetch_array($replyResult);
+        echo $replies['reply'] . " ";
+    }*/
+    echo "<br>";
+
+}
+
+function getReplies($parent, $uniqueID){
+    $connection = mysqli_connect('localhost', 'crypzzhj', 'D7iqck9yZMdr', 'crypzzhj_userlogin');
+    $result = mysqli_query($connection,"SELECT * FROM replies WHERE ID = '$ID'");
+    echo "Replies:", "<br>";
+    while($row = mysqli_fetch_array($result)) {
+        echo $row['reply'] . "<br>";
+    }
+}
+?>
+
+    
+    <br><br><br><br><br><br><br>
+
+
+
+</body>
+
+</html>
